079c93310babf72aafc0fbef27f5384f0f87743a
ibnutriyuono <ibnutriyuono@gmail.com>
2021-01-26 17:57:19 +0700

    add: fetch banner action

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,32 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from '../api/axios'
+// import router from '../router'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    banners: []
   },
   mutations: {
+    SET_BANNERS (state, payload) {
+      state.banners = payload
+    }
   },
   actions: {
+    fetchBanners (context, payload) {
+      axios({
+        method: 'GET',
+        url: '/banners'
+      })
+        .then(({ data }) => {
+          context.commit('SET_BANNERS', data)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
   },
   modules: {
   }
