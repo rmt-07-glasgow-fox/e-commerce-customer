8f50828fd025364a3c3f138fc6d4d805107a7c73
raflirach <rafli060395@gmail.com>
2021-01-26 11:17:15 +0700

    add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <navbar />
     <router-view/>
   </div>
 </template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+export default {
+  components: { Navbar },
+  name: 'App'
+}
+</script>
 
 <style>
 #app {
---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav class="navbar navbar-light bg-light px-3 fixed-top">
+    <div class="container-fluid row">
+      <a class="navbar-brand col-2">R-COMMERCE</a>
+      <form class="d-flex col-7">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      </form>
+      <div class="d-flex col-2 justify-content-evenly align-items-center">
+        <div><i class="fas fa-shopping-cart"></i></div>
+        <div><i class="fas fa-history"></i></div>
+        <button v-if="!access_token" class="btn btn-sm btn-outline-success">Login</button>
+        <div v-else><i class="fas fa-sign-out-alt"></i></div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      access_token: localStorage.access_token
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .fas{
+    font-size: 1.2em;
+  }
+</style>
