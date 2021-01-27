d8c868c45b8cc771a4347de70a3608d8c9654ef8
ical10 <mochhusnir@gmail.com>
2021-01-27 18:36:16 +0700

    creating register form

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -1,22 +1,28 @@
 <template>
     <div class='container'>
         <div class='row content'>
+            <div class='container'>
+                <div id="nav">
+                  <router-link to="/register"> Register </router-link> |
+                  <router-link to="/login"> Login </router-link>
+                </div>
+            </div>
             <div class='col-md-6 mb-3'>
                 <img src='../assets/images/signup-image.jpg' class='img-fluid' alt='image'>
             </div>
             <div class="col-md-6">
-                <h3 class='signin-text mb-3'> Register </h3>
-                <form>
-                    <div class="form-group">
-                        <label for="formGroupExampleInput">Email</label>
-                        <input type="email" class='form-control' id="formGroupExampleInput" placeholder="Type in your email...">
-                    </div>
-                    <div class="form-group">
-                        <label for="formGroupExampleInput2">Password</label>
-                        <input type="password" class='form-control' id="formGroupExampleInput2" placeholder="Type in your password...">
-                    </div>
-                    <button class='btn btn-class'>Register</button>
-                </form>
+                    <h3 class='signin-text mb-3'> Register </h3>
+                    <form>
+                        <div class="form-group">
+                            <label for="formGroupExampleInput">Email</label>
+                            <input type="email" class='form-control' id="formGroupExampleInput" placeholder="Type in your email...">
+                        </div>
+                        <div class="form-group">
+                            <label for="formGroupExampleInput2">Password</label>
+                            <input type="password" class='form-control' id="formGroupExampleInput2" placeholder="Type in your password...">
+                        </div>
+                        <button class='btn btn-class'>Register</button>
+                    </form>
             </div>
         </div>
     </div>
