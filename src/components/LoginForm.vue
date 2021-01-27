31141a1d311e4ff13d8374156483d9d20e37f458
ical10 <mochhusnir@gmail.com>
2021-01-27 18:35:57 +0700

    creating login forms

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,30 +1,53 @@
 <template>
     <div class='container'>
         <div class='row content'>
+            <div class='container'>
+                <div id="nav">
+                  <router-link to="/register"> Register </router-link> |
+                  <router-link to="/login"> Login </router-link>
+                </div>
+            </div>
             <div class='col-md-6 mb-3'>
                 <img src='../assets/images/signin-image.jpg' class='img-fluid' alt='image'>
             </div>
             <div class="col-md-6">
                 <h3 class='signin-text mb-3'> Login </h3>
-                <form>
+                <form @submit.prevent='login'>
                     <div class="form-group">
                         <label for="formGroupExampleInput">Email</label>
-                        <input type="email" class='form-control' id="formGroupExampleInput" placeholder="Type in your email...">
+                        <input type="email" class='form-control' id="formGroupExampleInput" placeholder="Type in your email..." v-model='email'>
                     </div>
                     <div class="form-group">
                         <label for="formGroupExampleInput2">Password</label>
-                        <input type="password" class='form-control' id="formGroupExampleInput2" placeholder="Type in your password...">
+                        <input type="password" class='form-control' id="formGroupExampleInput2" placeholder="Type in your password..." v-model='password'>
                     </div>
                     <button class='btn btn-class'>Login</button>
                 </form>
-            </div>
+        </div>
         </div>
     </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 export default {
-  name: 'LoginForm'
+  name: 'LoginForm',
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    login () {
+      const payload = {
+        email: this.email,
+        password: this.password
+      }
+      this.$store.dispatch('login', payload)
+    }
+  },
+  computed: mapState(['errors'])
 }
 </script>
 
