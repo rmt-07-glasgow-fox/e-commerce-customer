10895b8a4603868ca0918dd2944dda1f6947a341
ical10 <mochhusnir@gmail.com>
2021-01-27 18:38:56 +0700

    adding necessary attributes to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,73 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from '../api/axios'
+import router from '../router/'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    products: [],
+    username: '',
+    categories: [],
+    errors: []
   },
   mutations: {
+    insertProducts (state, payload) {
+      state.products = payload
+    },
+    getUserName (state, payload) {
+      state.username = payload.email.split('@')[0]
+    },
+    insertCategories (state, payload) {
+      const result = []
+      payload.forEach(product => {
+        result.push(product.category)
+      })
+      state.categories = [...new Set(result)]
+    },
+    catchError (state, payload) {
+      state.errors = []
+      state.errors.push(payload)
+    }
   },
   actions: {
+    addToCart (context, payload) {
+      return axios
+        .post('/carts', payload, {
+          headers: {
+            access_token: localStorage.getItem('access_token')
+          }
+        })
+    },
+    fetchProducts (context) {
+      axios
+        .get('/products', {
+          headers: {
+            access_token: localStorage.getItem('access_token')
+          }
+        })
+        .then(({ data }) => {
+          context.commit('insertProducts', data)
+          context.commit('insertCategories', data)
+        })
+        .catch(console.log)
+    },
+    login (context, payload) {
+      axios
+        .post('/login', payload)
+        .then(({ data }) => {
+          localStorage.setItem('access_token', data.access_token)
+          context.commit('getUserName', payload)
+          router.push('/main')
+        })
+        .catch((err) => {
+          context.commit('catchError', err.response.data.errors)
+        })
+    }
   },
   modules: {
-  }
+  },
+  plugins: [createPersistedState()]
 })
