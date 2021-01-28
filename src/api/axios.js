fe5c97e73045e92cbc3f62aa1b8876b29f66d7d6
Rivan-byte02 <rivanakbar02@gmail.com>
2021-01-28 11:08:16 +0700

    firebase deploy

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,6 @@ import axios from 'axios'
 
 const instance = axios.create({
   baseURL: 'https://e-commerce-cms-rivan.herokuapp.com/'
-  // baseURL: 'http://localhost:3000/'
 })
 
 export default instance
