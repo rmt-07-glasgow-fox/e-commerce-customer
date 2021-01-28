d776630349dc03de2de234655368e510b7135440
zaini01 <o.z41n1@gmail.com>
2021-01-28 13:08:37 +0700

    set baseUrl heroku

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  // baseURL: 'https://ecomersh8.herokuapp.com'
-  baseURL: 'http://localhost:3000'
+  baseURL: 'https://ecomersh8.herokuapp.com'
 })
 
 export default instance
